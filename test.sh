6ea466b3c4593615e86f0cd5e96eaae29b4a84c1
anaraya2 <arvind.narayanamurthy@intel.com>
2024-03-11 21:27:19 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ pwd
 arc shell python -- "which python && python --version"
 echo $USER
 whoami
+/usr/intel/bin/nbjob run --target zsc7_express --qslot /psg/psae/big/gen --class "SLES12SP5&&4C&&24G" --log-file-dir /nfs/site/disks/epg_etools_regressions/nblogs --log-file log_main.txt --mode interactive "ls pwd"
